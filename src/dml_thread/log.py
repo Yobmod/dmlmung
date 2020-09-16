20641d9d6abec0057df0e81830ec405d347b8638
yobmod <yobmod@gmail.com>
2020-09-16 14:10:43 +0100

    update pyinstaller

--- src/dml_thread/log.py
+++ src/dml_thread/log.py
@@ -3,15 +3,14 @@ import logging
 
 logger = logging.getLogger('DML E-Chem')
 logging.basicConfig(
-                    level=logging.INFO,  # DEBUG, INFO, WARNING, ERROR, CRITICAL
-                    format= '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-                    datefmt= '%d/%m/%Y %I:%M:%S %p',
-                    filename='./logs/dmlmung.log',
+    level=logging.INFO,  # DEBUG, INFO, WARNING, ERROR, CRITICAL
+    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+    datefmt='%d/%m/%Y %I:%M:%S %p',
+    filename='./logs/dmlmung.log',
 
-                    filemode='w'  # "a"
-                    )
+    filemode='w'  # "a"
+)
 
 # logging.debug('This message should go to the log file')
 # logging.warning('Watch out!')  # will print a message to the console
 # logging.info('I told you so')  # will not print anything
-
